6665e5f856d468da9e492f932bcd9e0fbc16a1e4
m.koltunov <m.koltunov@pixonic.com>
2020-02-08 17:14:10 +0300

    Improved MoneyStressTest

--- src/test/java/fintech/usecase/money/MoneyStressTest.java
+++ src/test/java/fintech/usecase/money/MoneyStressTest.java
@@ -2,50 +2,42 @@ package fintech.usecase.money;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MoneyStressTest extends AMoneyTest {
 
-    @Test
+    @Test(timeout = 15000L)
     public void stress() throws InterruptedException {
-        int accountCount = 10;
-        long startMoney = 1000L;
 
+        int accountCount = 3;
+        long startMoney = 100000L;
         IntStream.range(0, accountCount).forEach(it -> deposit(accountId(it), startMoney));
 
-        int iterationCount = 200;
+        int iterationCount = 1000;
         int threadCount = 15;
 
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         long trasferMoney = 1L;
 
-        List<Callable<Object>> tasks = IntStream.range(0, iterationCount)
-                .boxed()
-                .map(i -> IntStream.range(0, accountCount)
-                        .boxed()
-                        .map(index -> (Callable<Object>) () -> transfer(
-                                accountId(index),
-                                accountId((index + 1) % accountCount),
-                                trasferMoney
-                        ))
-                )
-                .reduce(Stream::concat)
-                .orElseThrow(RuntimeException::new)
-                .collect(Collectors.toList());
+        List<Callable<Object>> tasks = new ArrayList<>();
+        for (int i = 0; i < iterationCount; i++) {
+            for (int accountIndex = 0; accountIndex < accountCount; accountIndex++) {
+                final int index = accountIndex;
+                tasks.add(() -> transfer(accountId(index), accountId((index + 1) % accountCount), trasferMoney));
+            }
+        }
 
         executorService.invokeAll(tasks);
 
         IntStream.range(0, accountCount).forEach(it -> assertThat(state(accountId(it)).currentMoney, is(startMoney)));
-
     }
 
     private static String accountId(int index) {
